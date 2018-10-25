750197ecc0b421120259e2f20f28e01f37422800
Daniel Sutherland <danielmail40@gmail.com>
2018-10-25 16:26:11 +1100

    Rating Distribution functionality properly implemented

--- lib/SurveyApp.java
+++ lib/SurveyApp.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 public class SurveyApp {
 
     private static final int NUMBER_FILES = 2;
+    private static final int NUMBER_RATING_VALUES = 5;
+    private static final int MIN_RATING_VALUE = 1;
+    private static final int MAX_RATING_VALUE = 5;
 	
 	public static void main(String[] args) {
 	    if (!validInput(args)) {
@@ -25,14 +28,23 @@ public class SurveyApp {
                 calculateParticipation(surveyResponses);
         double[] ratingAverages = calculateAverages(surveyResponses,
                 surveyQuestions);
+        int[][] ratingDistributionResults =
+                calculateDistribution(surveyResponses, surveyQuestions);
+
+        // TODO - verify that participationResult is not null
 
         System.out.println("Participation percentage: "
                 + (100 * participationResult.getParticipationPercentage())
                 + "%");
+
         System.out.println("Participation count: "
                 + participationResult.getParticipationCount());
+
         System.out.println("Average Ratings: ");
-        printArray(ratingAverages);
+        printRatings(ratingAverages);
+
+        System.out.println("Rating Distributions (Per Rating Question):");
+        printRatingDistributions(ratingDistributionResults);
 	}
 
     /**
@@ -72,9 +84,64 @@ public class SurveyApp {
     }
 
     /**
+     * For each rating question, find the distribution of rating values.
+     * Note: It may be desirable to store this distribution data somewhere (i.e. in the question object)
+     * @param surveyResponses
+     * @param surveyQuestions
+     * @return 2d array, of dimensions: nukber of dimensions * number of rating values
+     */
+    private static int[][] calculateDistribution(ArrayList<Response> surveyResponses,
+                                                 ArrayList<Question> surveyQuestions) {
+        int numberOfRatingQuestions = numberOfQuestionsOfType(surveyQuestions,
+                QuestionType.RATING);
+
+        // TODO - make constant
+        int[][] distributionResults = new int[numberOfRatingQuestions][NUMBER_RATING_VALUES];
+
+        ArrayList<Integer> ratings;
+
+        int responseNumber = 0;
+        for (Response response : surveyResponses) {
+
+            // If this response is unsubmitted, don't consider it
+            if (!response.getIsSubmitted()) {
+                continue;
+            }
+            ratings = response.getRatings();
+
+            // For each rating question
+            for (int ratingQuestionNumber = 0;
+                 ratingQuestionNumber < numberOfRatingQuestions;
+                 ratingQuestionNumber++) {
+
+                // If this rating question is unanswered, don't consider it
+                if (ratings.get(ratingQuestionNumber) == Response.EMPTY_RATING) {
+                    continue;
+                }
+
+                int ratingValue =
+                        response.getRatings().get(ratingQuestionNumber);
+
+                // if the rating value is invalid, don't consider it
+                if (ratingValue < MIN_RATING_VALUE || ratingValue > MAX_RATING_VALUE) {
+                    continue;
+                }
+
+                // -1 since array's are 0-indexed
+                distributionResults[ratingQuestionNumber][ratingValue - 1]++;
+            }
+
+            responseNumber++;
+        }
+
+	    return distributionResults;
+    }
+
+    /**
      * Calculates the average value for each rating question
      * @param surveyResponses   an ArrayList of survey Response objects
-     * @return                  an array of containing each rating average
+     * @param surveyQuestions   an ArrayList of survey Question objects
+     * @return                  an array containing each rating average
      */
     protected static double[] calculateAverages(
             ArrayList<Response> surveyResponses,
@@ -141,19 +208,38 @@ public class SurveyApp {
     }
 
     /**
-     * Print an array of double values
+     * Print an array of (double typed) rating values
      * @param values    array of doubles
      */
-    private static void printArray(double[] values) {
+    private static void printRatings(double[] values) {
         if (values == null) {
             System.out.println("Array is empty.");
             return;
         }
         int ratingNumber = 1;
         for (double value : values) {
-            System.out.println(ratingNumber + ": " + value);
+            System.out.println("RatingQ #" + ratingNumber + " : " + value);
             ratingNumber++;
         }
     }
+
+    /**
+     * For each rating question, print the distribution of ratings
+     * @param ratingDistributionResults
+     */
+    private static void printRatingDistributions(int[][] ratingDistributionResults) {
+        String output;
+        for (int ratingNumber = 0;
+             ratingNumber < ratingDistributionResults.length; ratingNumber++) {
+            output = "RatingQ #" + (ratingNumber + 1) + " : ";
+
+            for (int value = 0; value < MAX_RATING_VALUE; value++) {
+                output += ratingDistributionResults[ratingNumber][value] + " ";
+            }
+
+            System.out.println(output);
+        }
+    }
+
 
 }
